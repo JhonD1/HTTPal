efc8cc552bcc4383ba9e9b59ac462758e41f0a82
JD <johnishuman@gmail.com>
2022-11-02 23:57:51 +0800

    getting input url from user

--- lib/httpal.rb
+++ lib/httpal.rb
@@ -1,7 +1,9 @@
 require 'net/http'
 
-uri = URI('https://openlibrary.org/books/OL7353617M.json')
+p "Enter URL to get json from: "
+user_input = gets.chomp.to_s
+
+uri = URI(user_input)
 res = Net::HTTP.get_response(uri)
 
-puts res
 puts res.body 
